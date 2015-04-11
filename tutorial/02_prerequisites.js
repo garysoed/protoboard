4cf0218da30d6f85cd8068b6a216a2466435f3ef
Gary Soedarsono <garysoed@gmail.com>
2015-04-11 12:23:41 -0700

    fixed tutorial

--- tutorial/02_prerequisites.js
+++ tutorial/02_prerequisites.js
@@ -7,7 +7,7 @@
  * 3. Protoboard relies on
  *    [HTML Import](http://www.html5rocks.com/en/tutorials/webcomponents/imports/). So you will need
  *    an HTTP server serving static files. A simple one you can use is
- *    [http-server](https://www.npmjs.com/package/http-server)":
+ *    [http-server](https://www.npmjs.com/package/http-server):
  *    `npm install http-server`.
  * 4. Run `http-server` from the root directory of the project, and navigate to the file you want to
  *    open. A good one to start is `ex/all/index.html`.
