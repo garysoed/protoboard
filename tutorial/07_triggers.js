e56291084d92e0fcf06f45793f6f82f129c7a937
Gary Soedarsono <garysoed@gmail.com>
2015-06-25 23:00:51 -0700

    added support for adding custom triggers. Fixes #84 and #83

---
+++ tutorial/07_triggers.js
@@ -0,0 +1,43 @@
+/**
+ * Beside abilities, Protoboard also allows you to create your own Triggers. Triggers are the main
+ * way for Abilities to be executed. Examples of Triggers include the
+ * {{#crossLink "trigger.Click"}}Click{{/crossLink}} trigger, which triggers an ability on an
+ * element when the user clicks that element.
+ *
+ * To create a trigger, you need to implement the {{#crossLink "trigger.Trigger"}}{{/crossLink}}
+ * class. You will then need to register a parser with the
+ * {{#crossLink "trigger.Triggers"}}{{/crossLink}} service, by calling the
+ * {{#crossLink "trigger.Triggers/register:method"}}register{{/crossLink}} method. For example:
+ * ```javascript
+ * DI.run({ Events: '=', Trigger: 'trigger.=', Triggers: 'trigger.=' }, function($i) {
+ *   var OnMouseUpTrigger = function() {};
+ *   OnMouseUpTrigger.prototype = Object.create($i.Trigger.prototype);
+ *
+ *   OnMouseUpTrigger.prototype.on = function(element, ability) {
+ *     $i.Events.of(element, this)
+ *         .on('dom', 'mouseup', ability.trigger.bind(ability, element));
+ *   };
+ *
+ *   OnMouseUpTrigger.prototype.off = function(element) {
+ *   	 $i.Events.of(element, this).off('dom', 'mouseup');
+ *   };
+ *
+ *   OnMouseUpTrigger.prototype.toString = function() {
+ *     return 'mouseup';
+ *   };
+ *
+ *   OnMouseUpTrigger.prototype.helpText = 'Triggers when mouse is unpressed on the element';
+ *
+ *   $i.Triggers.register(function(string) {
+ *     return (string === 'mouseup') ? new OnMouseUpTrigger() : undefined;
+ *   });
+ * });
+ * ```
+ *
+ * The {{#crossLink "Events"}}{{/crossLink}} class is a utility class to make event registering and
+ * unregistering easier to manage.
+ *
+ * @class 7 Triggers
+ * @since 1.0.0
+ * @module tutorial
+ */
